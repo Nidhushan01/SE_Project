cea7f2471f06869013af91770e10086804157bfc
Nidhushan01 <nidhu1213@gmail.com>
2025-02-01 15:22:36 +0530

    databade setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
+dotenv.config();
+
+console.log(process.env.MONGO_URI);
 const app = express();
 
+app.get('/products',(req,res)=>{
+    res.send("hello world")
+    
+})
+
+
 app.listen(5000,() =>{
+    connectDB();
     console.log("server connected");
 }
 );
